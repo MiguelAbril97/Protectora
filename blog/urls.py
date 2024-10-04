0960e53098470b9c89c9ca6e01ce48046a44cf22
Miguel Abril <miguel.abril.alamo.al@iespoligonosur.org>
2024-10-04 13:46:36 +0200

    añado una url vacia

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.entradaAnimales, name='entradaAnimales.html'),
     path('animales/', views.entradaAnimales, name='entradaAnimales.html'),
     path('colaboradores/', views.entradaColaboradores, name='entradaColaboradores.html'),
     path('protectoras/', views.entradaProtectoras, name='entradaProtectoras.html'),
